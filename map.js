3f6e3acda2d547b973e7d54b0c568fa4d12b56c5
Jafer Ahmed <jmahmed@usc.edu>
2021-04-17 23:51:25 -0700

    organized files and fixed marker bug

--- map.js
+++ map.js
@@ -1,6 +1,5 @@
 
 async function getMeterStatus(latitude, longitude) {
-    addSearchBar();
     var parkingMeterRepository = new ParkingMeterRepository();
     var vacantParkingMeterList = await parkingMeterRepository.fetchVacantMeters(latitude,longitude);
     if(!vacantParkingMeterList || vacantParkingMeterList.length == 0) console.log("Unfortunately no meter spots are free at the moment.")
@@ -10,34 +9,39 @@ async function getMeterStatus(latitude, longitude) {
     }  
 }
 
-
-function addSearchBar(){
-    var map;
-    //$(document).ready(function(){
-        mapboxgl.accessToken = MapBoxKey;
-        map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [LosAngelesLong,LosAngelesLat],
-        zoom: 8
-        });
-
-    var geocoder = new MapboxGeocoder({ // Initialize the geocoder
-        accessToken: mapboxgl.accessToken, // Set the access token
-        mapboxgl: mapboxgl, // Set the mapbox-gl instance
+function initializeMap(){
+    mapboxgl.accessToken = MapBoxKey;
+                map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [LosAngelesLong,LosAngelesLat],
+                zoom: 13
     });
-
-    // Add the geocoder to the map
+}
+function addSearchBar(){
+    var geocoder = new MapboxGeocoder({ // Initialize the geocoder
+        accessToken: mapboxgl.accessToken, 
+        mapboxgl: mapboxgl, 
+    });
+        // Add the geocoder to the map
     map.addControl(geocoder);
     geocoder.on('result', function (e) {
-        var marker2 = new mapboxgl.Marker({ color: 'red', rotation: 45 })
-        .setLngLat([e.result.geometry.coordinates[0], e.result.geometry.coordinates[1]])
-        .addTo(map);
         getMeterStatus(e.result.geometry.coordinates[1],e.result.geometry.coordinates[0]);
     });
+}
 
-//});
-    
+function addZoomToMap(){
+    //Add zoom to map
+    var nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
 
 }
+
+function setupMap(){
+    initializeMap();
+    addSearchBar();
+    addZoomToMap();
+}
+
+
 
